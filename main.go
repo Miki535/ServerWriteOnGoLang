805ddb2de9084d41e8879d573982d62c04792ca3
Taras <sherbantaras535@gmail.com>
2023-12-01 12:58:34 +0200

    .

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func htmlPasre(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("teamplates/index.html")
+	tmpl.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", htmlPasre)
+	http.ListenAndServe(":8080", nil)
+}
